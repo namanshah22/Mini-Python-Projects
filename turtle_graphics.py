b040033bdccadc31fdf210b9b0e0c5b9f0f6d27e
naman shah <93568656+namanshah22@users.noreply.github.com>
2023-04-08 11:11:42 +0530

    Add files via upload

---
+++ turtle_graphics.py
@@ -0,0 +1,12 @@
+import turtle
+turtle.bgcolor("black")
+turtle.pensize(5)
+turtle.speed(0.1)
+colours=["green","red","aqua","yellow","blue","magenta"]
+for i in range(20):
+    for color in colours:
+        turtle.color(color)
+        turtle.circle(120)
+        turtle.right(10)
+    
+turtle.mainloop()
